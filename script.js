232b7d2e89428c4c64135a194609ac48909bf2b1
ease <easeanger@gmail.com>
2022-04-17 03:31:13 +0530

    setting height  on input simultaneously

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ const input = document.getElementById('input');
 console.log(input)
 const setHeight = 
   (e) => {
-    if (e.target.value > 100) {
+    if (e.target.value > 0) {
       if (e.target.value > 1500) {
         return alert("please enter a value below 1500px")
       }
       height = e.target.value;
       svgBox.style.height = `${ height }px`;
     } else {
-      alert('Please Enter a value above 100px')
+      alert('Please Enter a value above 0px')
     }
   
   }
 ///adding event listener to input
-input.addEventListener("change",(e)=>setHeight(e))
+input.addEventListener("input",(e)=>setHeight(e))
